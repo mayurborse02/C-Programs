45a66af0ca5a8c45d9ba1c176c2cdac9ea298c86
Mr Mayur borse <mayurborse880@gmail.com>
2026-05-30 10:12:10 +0530

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+
+         printf("Jay Ganesha...!\n");
+
+
+         return 0;
+
+    
+}
---
+++ sizeOf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99f;
+    double d = 78.48573459;
+
+    printf("Size of all variables.\n");
+    printf("%lu\n",sizeof(ch));  //1-byte
+    printf("%lu\n",sizeof(i));   //4-byte
+    printf("%lu\n",sizeof(f));   //4-byte
+    printf("%lu\n",sizeof(d));  //8-byte
+    
+
+
+    
+    return 0;
+}
